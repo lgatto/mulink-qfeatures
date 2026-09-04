40547d3ad9461885d9be62bf94224330fcb917d4
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-09-04 14:47:38 +0200

    add some validation

--- make-data_brunner2022.R
+++ make-data_brunner2022.R
@@ -5,4 +5,33 @@ source("R/io_converter.R")
 
 scp <- brunner2022()
 
-writeLinkH5MU(scp, "brunner.h5mu")
+writeLinkH5MU(
+  scp,
+  "brunner2022.h5mu",
+  feature_mapping_key = "Protein.Group"
+)
+
+scp_from_h5mu <- readLinkH5MU(
+  "brunner2022.h5mu",
+  feature_mapping_key = "Protein.Group"
+)
+# Warning message:
+# sampleMap[['assay']] coerced with as.factor()
+
+## some validation
+length(scp) == length(scp_from_h5mu)
+
+old_assay <- assay(scp, length(scp))
+new_assay <- assay(scp_from_h5mu, length(scp_from_h5mu))
+all.equal(old_assay, new_assay, check.attributes = TRUE)
+# TRUE
+
+old_rowdata <- rowData(scp)[[length(scp)]]
+new_rowdata <- rowData(scp_from_h5mu)[[length(scp_from_h5mu)]]
+all.equal(old_rowdata, new_rowdata, check.attributes = TRUE)
+# TRUE
+
+old_coldata <- colData(scp)
+new_coldata <- colData(scp_from_h5mu)
+all.equal(old_coldata, new_coldata, check.attributes = TRUE)
+# TRUE
